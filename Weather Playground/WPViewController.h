8a76cfed21d320d38c8aa11b6567d93a01bef177
SiEOC <skyler@whitecapinstitute.com>
2015-05-25 17:28:16 -0600

    this has a bug but displays stuff in the console okay

--- Weather Playground/WPViewController.h
+++ Weather Playground/WPViewController.h
@@ -10,4 +10,19 @@
 
 @interface WPViewController : UIViewController
 
+// buttons for StoryBoards
+
+@property (strong, nonatomic) IBOutlet UITextField *searchField;
+
+@property (strong, nonatomic) IBOutlet UILabel *countryName;
+
+@property (strong, nonatomic) IBOutlet UIImageView *iconImageView;
+
+@property (strong, nonatomic) IBOutlet UILabel *tempLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *mainLabel;
+//@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+
+- (IBAction)searchButtonPressed:(UIButton *)sender;
+
 @end
--- Weather Playground/WPViewController.m
+++ Weather Playground/WPViewController.m
@@ -18,18 +18,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
     //[[NetworkController sharedInstance] getWeatherWithName:@"London,uk"];
     //Weather *weather = [Weather new];
     //[weather getWeatherWithName:@"Salt/%20Lake/%20City,Utah"];
-    WeatherController *weatherController = [WeatherController sharedInstance];
+/*    WeatherController *weatherController = [WeatherController sharedInstance];
     [weatherController getWeatherWithName:@"London,uk" completion:^(Weather *weather)
      {
          NSLog(@"weather=\r%@", weather);
      }
-     ];
+     ];*/
     
     //- (void)someMethodThatTakesABlock:(returnType (^)(parameterTypes))blockName;
+}
+// Start process of weather
+- (IBAction)searchButtonPressed:(UIButton *)sender
+{
+    WeatherController *weatherController = [WeatherController sharedInstance];
+    [weatherController getWeatherWithName:self.searchField.text completion:^(Weather *weather)
+     {
+         NSLog(@"weather=\r%@", weather);
+         self.countryName.text = weather.locationName;
+         //self.iconImageView = weather;
+         self.tempLabel.text = weather.weatherTemp;
+         self.mainLabel.text = weather.weatherMain;
+         //self.
+         NSLog(@"done");
+     }
+     ];
+    
+  /*
+   operty (weak, nonatomic) IBOutlet UITextField *searchField;
+   
+   @property (weak, nonatomic) IBOutlet UILabel *countryName;
+   
+   @property (weak, nonatomic) IBOutlet UIImageView *iconImageView;
+   
+   @property (weak, nonatomic) IBOutlet UILabel *tempLabel;
+   
+   @property (weak, nonatomic) IBOutlet UILabel *mainLabel;
+   
+   */
+     
 }
 
 - (void)didReceiveMemoryWarning
